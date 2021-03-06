33029ac7d27a739ba301abbf1014c16b7e81182f
Dmitry <79753416+ZloyClown@users.noreply.github.com>
2021-03-06 19:01:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-f = open('BD/bd1.txt', 'w')
-f.write('sasat')
-f.close()
-for i in range(20):
-    f = open(f"{i}", 'w')
-    f.write(f"{random.randint(1, 1000)}")
-    f.close()
+# import random
+# f = open('BD/bd1.txt', 'w')
+# f.write('sasat')
+# f.close()
+# for i in range(20):
+#     f = open(f"{i}", 'w')
+#     f.write(f"{random.randint(1, 1000)}")
+#     f.close()
